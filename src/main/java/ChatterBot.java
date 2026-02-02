149c0c7d5cdcbb4063b19f70465c1a8e6069d95f
siaoarh <elluimelliot@gmail.com>
2026-02-02 23:59:07 +0800

    Level 1

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class ChatterBot {
     public static void main(String[] args){
         System.out.println("Hello I am ChatterBot! I was created by github user Siaoarh");
         System.out.println("How can I help you today?");
-        System.out.println("Goodbye!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")){
+                System.out.println("Goodbye!");
+                break;
+            }
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
