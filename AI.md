9ec446ff547525d228b6af03180ed97ccba10a50
siaoarh <elluimelliot@gmail.com>
2026-02-02 23:50:38 +0800

    Level 0

---
+++ AI.md
@@ -0,0 +1,1 @@
+Github Copilot used for Level 0.
---
+++ src/main/java/ChatterBot.java
@@ -0,0 +1,7 @@
+public class ChatterBot {
+    public static void main(String[] args){
+        System.out.println("Hello I am ChatterBot! I was created by github user Siaoarh");
+        System.out.println("How can I help you today?");
+        System.out.println("Goodbye!");
+    }
+}
