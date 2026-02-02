8b41ca02c7a5f4c4d9c8eaadd86a5ed819e9ccae
siaoarh <elluimelliot@gmail.com>
2026-02-03 00:22:15 +0800

    Level 3

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -8,6 +8,7 @@ public class ChatterBot {
         Scanner scanner = new Scanner(System.in);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         while (true){
@@ -19,12 +20,35 @@ public class ChatterBot {
             }
 
             if (input.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++){
-                    System.out.print((i + 1) + ". " + tasks[i] + "\n");
+                    System.out.print((i + 1) + ".[" + (isDone[i] ? "X" : " ") + "] " + tasks[i] + "\n");
                 }
                 continue;
             }
+
+             if (input.startsWith("mark ")) {
+                int index = parseIndex(input, "mark ");
+                if (index >= 1 && index <= taskCount) {
+                    isDone[index - 1] = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [" + (isDone[index - 1] ? "X" : " ") + "] " + tasks[index - 1]);
+                }
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int index = parseIndex(input, "unmark ");
+                if (index >= 1 && index <= taskCount) {
+                    isDone[index - 1] = false;
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  [" + (isDone[index - 1] ? "X" : " ") + "] " + tasks[index - 1]);
+                }
+                continue;
+            }
+
             tasks[taskCount] = input;
+            isDone[taskCount] = false;
             taskCount++;
 
             System.out.println("added: " + input);
@@ -32,4 +56,13 @@ public class ChatterBot {
 
         scanner.close();
     }
+
+    private static int parseIndex(String input, String command) {
+        try {
+            return Integer.parseInt(input.substring(command.length()).trim());
+        } catch (NumberFormatException e) {
+            return -1; // Invalid index
+        }
+    }
+
 }
