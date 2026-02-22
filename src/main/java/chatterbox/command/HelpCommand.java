db7b3bbd5493f5fab37c6d857714cc82d9d276c8
siaoarh <elluimelliot@gmail.com>
2026-02-22 16:57:32 +0800

    Updated HelpCommand

--- src/main/java/chatterbox/command/HelpCommand.java
+++ src/main/java/chatterbox/command/HelpCommand.java
@@ -1,30 +1,56 @@
 package chatterbox.command;
-
 import chatterbox.Storage;
 import chatterbox.TaskList;
 
 /**
- * A command that shows available commands.
+ * A command that displays usage instructions and available commands.
  */
 public class HelpCommand implements Command {
 
+    private static final String HELP_MESSAGE = """
+            ChatterBox Help Guide
+
+            ChatterBox helps you manage and track your tasks.
+
+            Available commands:
+
+            list
+                Displays all tasks.
+
+            todo <description>
+                Adds a todo task.
+
+            deadline <description> /by <date>
+                Adds a deadline task.
+
+            event <description> /from <date> /to <date>
+                Adds an event task.
+
+            mark <index>
+                Marks a task as done.
+
+            unmark <index>
+                Marks a task as not done.
+
+            delete <index>
+                Deletes a task.
+
+            find <keyword>
+                Finds tasks containing a keyword.
+
+            cheer
+                Displays a motivational message.
+
+            help
+                Shows this help message.
+
+            bye
+                Exits the application.
+            """;
+
     @Override
     public CommandResult execute(TaskList tasks, Storage storage) {
-        String helpMessage = """
-                Available commands:
-                - list
-                - todo <description>
-                - deadline <description> /by <date>
-                - event <description> /from <date> /to <date>
-                - mark <index>
-                - unmark <index>
-                - delete <index>
-                - find <keyword>
-                - cheer
-                - bye
-                """;
-
-        return new CommandResult(helpMessage.trim());
+        return new CommandResult(HELP_MESSAGE);
     }
 
     @Override
